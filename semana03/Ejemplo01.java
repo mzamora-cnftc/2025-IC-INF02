c6dc2f1200f739397724db4e76803c3589094940
mauricio-zamora <141055068+mzamora-cnftc@users.noreply.github.com>
2025-02-07 01:03:23 +0000

    Semana 03

---
+++ semana03/Ejemplo01.java
@@ -0,0 +1,25 @@
+package semana03;
+
+import generico.Utils;  // sin esta línea no pueden usar el Utils
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+
+        // limpiar
+        Utils.limpiarConsola();
+
+        // Declaración de variab les
+        int numero;
+
+        // Inputs
+        numero = Utils.leerEntero("Digite un número para evaluar: ");
+
+        // Process
+        if (numero < 0) {
+            numero = numero * -1;
+        }
+
+        // Output
+        System.out.println("El valor absoluto es: " + numero);
+    }
+}
