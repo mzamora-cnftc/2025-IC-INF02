c912ec2939575f9a11d1ac40702277d1b233c5b0
mauricio-zamora <141055068+mzamora-cnftc@users.noreply.github.com>
2025-02-19 02:36:15 +0000

    String

---
+++ semana05/EjemploString.java
@@ -0,0 +1,51 @@
+package semana05;
+
+import generico.Utils;
+
+public class EjemploString {
+
+    public static void main(String[] args) {
+        String compañera1 = "Abigail", compañera2 = "  María    Alexandra  ", compañero1 = "cristopher", compañero2 = "Daniel";
+        String sinNada = null;
+
+        Utils.limpiarConsola();
+
+        System.out.println(compañera1);
+        System.out.println(compañera1.toUpperCase());
+        System.out.println(compañera1.toLowerCase());
+        System.out.println(compañera1);
+
+        System.out.println(compañera2);
+        System.out.println(compañera2.trim());
+        System.out.println(compañera2);
+        compañera2 = compañera2.trim();
+        System.out.println(compañera2);
+
+        //if (compañero1 == "cristopher")  /NO HACER ESTO PARA COMPARAR EL VALOR
+        if (compañero1.equals("Cristopher")) {
+            System.out.println("Son iguales");
+        } else {
+            System.out.println("No son iguales");
+        }
+
+        if (compañero1.equalsIgnoreCase("Cristopher")) {
+            System.out.println("Son iguales");
+        } else {
+            System.out.println("No son iguales");
+            
+        }
+
+        if ("hola".equals(sinNada)) {
+            System.out.println("Si entro :)");
+        } else {
+            System.out.println("No entro al null");
+        }
+
+        if (compañera1.equalsIgnoreCase(compañera2)) {
+            System.out.println("Son iguales las compañeras");
+        } else {
+            System.out.println("No son iguales las compañeras");
+        }
+
+    }
+}
