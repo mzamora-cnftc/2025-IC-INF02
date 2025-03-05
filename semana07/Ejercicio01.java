6fb8757b210b7d0b230605a5426ead66e30f7248
mauricio-zamora <141055068+mzamora-cnftc@users.noreply.github.com>
2025-03-05 01:48:42 +0000

    Semana 07

---
+++ semana07/Ejercicio01.java
@@ -0,0 +1,16 @@
+package semana07;
+
+import generico.Utils;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        int n;
+
+        do {
+            n = Utils.leerEntero("Digite el número: ");
+        } while (n % 2 == 0);
+
+        System.out.println("Muchas gracias");
+
+    }
+}
